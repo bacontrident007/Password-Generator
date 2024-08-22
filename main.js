27deb89022c8cafcbffc1751aa988f3a1480c267
bacontrident007 <141420235+bacontrident007@users.noreply.github.com>
2024-08-22 17:35:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+
+let tamanhoSenha = document.querySelector(".password-size-number");
+const checkbox = document.querySelectorAll(".password-checkbox")
+const maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const minusculas = "abcdefghijklmnopqrstuvwxyz"
+const numeros = "01234567890123456789"
+const simbolos = "!@#$%&*():;><=+-_?/~[]{}^|"
+const nivelSenha = document.querySelector(".password-strenght")
+
+function botaoMais(){
+    if (tamanhoSenha.textContent < 20){
+        tamanhoSenha.textContent++;
+        geraSenha();
+    }
+}
+
+function botaoMenos(){
+    if (tamanhoSenha.textContent > 4){
+        tamanhoSenha.textContent--;
+        geraSenha();
+    }
+}
+
+function geraSenha(){
+    alfabeto = ""
+
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + maiusculas
+    }
+    
+    if(checkbox[1].checked){
+        alfabeto = alfabeto + minusculas
+    }
+    
+    if(checkbox[2].checked){
+        alfabeto = alfabeto + numeros
+    }
+    
+    if(checkbox[3].checked){
+        alfabeto += simbolos
+    }
+
+    let senha = ""
+    for (i=1;i<=tamanhoSenha.textContent;i++){
+        let numeroAleatorio = alfabeto[Math.floor(Math.random()*alfabeto.length)]    
+        senha = senha + numeroAleatorio
+    }
+    document.querySelector(".principal-output").value = senha
+    let forcaSenha = Math.log10(alfabeto.length**tamanhoSenha.textContent)
+    classificaSenha(forcaSenha);
+}
+
+geraSenha();
+
+console.log(nivelSenha)
+
+function classificaSenha(forcaSenha){
+    nivelSenha.classList.remove("weak")
+    nivelSenha.classList.remove("media")
+    nivelSenha.classList.remove("strong")
+    if (forcaSenha < 15) {
+        nivelSenha.classList.add("weak")
+    } else if ( 25 > forcaSenha && forcaSenha > 15) {
+        nivelSenha.classList.add("media")
+    } else {
+        nivelSenha.classList.add("strong")
+    }
+
+}
